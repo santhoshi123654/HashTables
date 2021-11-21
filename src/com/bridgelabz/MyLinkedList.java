db7cd17034df5eae9d6194040005f0196c42ffdc
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-21 16:12:11 +0530

    Added: MyLinkedList Program

---
+++ src/com/bridgelabz/MyLinkedList.java
@@ -0,0 +1,36 @@
+package com.bridgelabz;
+
+public class MyLinkedList<K> {
+
+	private INode<K> head;
+	private INode<K> tail;
+
+	public INode<K> search(K key) {
+		INode<K> temp = head;
+		while (temp != null) {
+			if (temp.getKey().equals(key)) {
+				return temp;
+			}
+			temp = temp.getNext();
+		}
+		return null;
+	}
+
+	public void append(INode<K> myNode) {
+
+		if (this.head == null) {
+			this.head = myNode;
+		}
+		if (this.tail == null) {
+			this.tail = myNode;
+		} else {
+			this.tail.setNext(myNode);
+			this.tail = myNode;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "MyLinkedList [head=" + head + "]";
+	}
+}
