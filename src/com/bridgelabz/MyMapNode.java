c45a3aa296678c62eaaaa28ce54b8e11a7667daf
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-21 16:36:13 +0530

    Added:The removal of word avoidable in para

---
+++ src/com/bridgelabz/MyMapNode.java
@@ -0,0 +1,60 @@
+package com.bridgelabz;
+
+public class MyMapNode <K, V> implements INode<K> {
+
+	private K key;
+	private V value;
+	private INode<K> next;
+
+	public MyMapNode(K key, V value) {
+		super();
+		this.key = key;
+		this.value = value;
+		this.next = null;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+
+	@Override
+	public K getKey() {
+
+		return this.key;
+	}
+
+	@Override
+	public void setKey(K key) {
+		
+		this.key = key;
+	}
+
+	@Override
+	public void setNext(INode<K> next) {
+		
+		this.next = next;
+
+	}
+
+	@Override
+	public INode<K> getNext() {
+		return next;
+	}
+
+	@Override
+	
+	
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("MyNode{key=").append(key).append("}");
+		if(next != null) {
+			buffer.append("->").append(next);
+		}
+		return buffer.toString();
+	}
+
+}
