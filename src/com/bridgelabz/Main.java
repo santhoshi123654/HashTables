3946084c213048271e47d7e908029b7b8771a337
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-21 16:33:56 +0530

    Added:The removal of word avoidable in para

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String string = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		String[] words = string.split(" ");
+
+		HashMap<String, Integer> linkedHashMap = new HashMap<>();
+
+		for (int i = 0; i < words.length; i++) {
+
+			String word = words[i];
+			Integer value = linkedHashMap.get(word);
+			if (value == null) {
+				value = 1;
+			} 
+			else {
+				value += 1;
+			}
+			linkedHashMap.put(word, value);
+		}
+		System.out.println(linkedHashMap);
+		linkedHashMap.remove("avoidable");
+		System.out.println(linkedHashMap.get("avoidable"));
+	}
+}
